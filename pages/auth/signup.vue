fdafb8320c0e65e086ae9915b0960d239ab00886
applandsys <applandsys@gmail.com>
2025-04-07 23:20:22 +0600

    Location in signup

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
   import { NuxtLink } from '#components';
-  import { IonInput, IonItem, IonLabel  } from '@ionic/vue';
+import { IonPage,  IonSelect, IonSelectOption } from '@ionic/vue'
   import AuthLayout from '~/layouts/auth.vue';
 
   const { $api } = useNuxtApp();
 
-  const formData = ref({
+const divisions = ref([]);
+const districts = ref([]);
+const subdistricts = ref([]);
+const unions = ref([]);
+
+const selectedDivision = ref(null);
+const selectedDistrict = ref(null);
+const selectedSubDistrict = ref(null);
+const selectedUnion = ref(null);
+
+const formData = ref({
     name: "",
     email: "",
     password: "",
     c_password: ""
   });
-
-const divisions = ref([]);
 
 const getDivision = async () => {
         try {
@@ -23,9 +31,40 @@ const getDivision = async () => {
         }
     };
 
-    onMounted(()=>{
-      getDivision();
-    });
+
+onMounted(()=>{
+  getDivision();
+});
+
+
+watch(selectedDivision, async (divisionId) => {
+    try {
+        const response = await $api.get(`/locations/district/${divisionId}`)
+        districts.value = response.data.districts;
+      } catch (error) {
+        console.error('API Error:', error)
+      }
+});
+
+watch(selectedDistrict, async (districtId) => {
+    try {
+        const response = await $api.get(`/locations/subdistrict/${districtId}`)
+        subdistricts.value = response.data.subdistricts;
+      } catch (error) {
+        console.error('API Error:', error)
+      }
+});
+
+watch(selectedSubDistrict, async (subDistrictId) => {
+    try {
+        const response = await $api.get(`/locations/union/${subDistrictId}`)
+        unions.value = response.data.unions;
+      } catch (error) {
+        console.error('API Error:', error)
+      }
+});
+
+
 
 
 </script>
@@ -58,10 +97,55 @@ const getDivision = async () => {
                 </ion-item>
 
                 <ion-item>
-                    <ion-select label="Division">
-                      <ion-select-option :value="division.id" v-for="division in divisions?.divisions" :key="division.id">{{ division.name }}</ion-select-option>
-                    </ion-select>
+                  <ion-select label="Division" v-model="selectedDivision">
+                    <ion-select-option
+                      :value="division.id"
+                      v-for="division in divisions?.divisions"
+                      :key="division.id"
+                    >
+                      {{ division.name }}
+                    </ion-select-option>
+                  </ion-select>
                 </ion-item>
+
+            
+                <ion-item v-if="districts.length" >
+                  <ion-select label="District" v-model="selectedDistrict" >
+                    <ion-select-option
+                      :value="district.id"
+                      v-for="district in districts"
+                      :key="district.id"
+                    >
+                      {{ district.name }}
+                    </ion-select-option>
+                  </ion-select>
+                </ion-item>
+
+                <ion-item v-if="subdistricts.length" >
+                  <ion-select label="Upazilla" v-model="selectedSubDistrict">
+                    <ion-select-option
+                      :value="subdistrict.id"
+                      v-for="subdistrict in subdistricts"
+                      :key="subdistrict.id"
+                    >
+                      {{ subdistrict.name }}
+                    </ion-select-option>
+                  </ion-select>
+                </ion-item>
+
+            
+                <ion-item v-if="unions.length" >
+                  <ion-select label="Union" v-model="selectedUnion">
+                    <ion-select-option
+                      :value="union.id"
+                      v-for="union in unions"
+                      :key="union.id"
+                    >
+                      {{ union.name }}
+                    </ion-select-option>
+                  </ion-select>
+                </ion-item>
+
 
                 <ion-button expand="full" type="submit"  shape="round" color="success">Sinup</ion-button>
             </form>
